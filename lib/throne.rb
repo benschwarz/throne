5a67cacf7e44a9c089793cfdd645405591123c1e
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-26 18:17:15 +1100

    Server setup is now handled by Database

--- lib/throne.rb
+++ lib/throne.rb
@@ -1,16 +1,11 @@
 require 'rest_client'
 require 'hashie'
 require 'yajl'
+
 module Throne
   autoload :Tasks,    'throne/tasks'
   autoload :Request,  'throne/request'
   autoload :Document, 'throne/document'
   autoload :Database, 'throne/database'
   autoload :DesignDocument, 'throne/design_document'
-  
-  class << self
-    attr_accessor :server, :database
-  end
-  
-  self.server = "http://127.0.0.1:5984"
 end
