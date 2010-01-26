7b9e35707d706a879f12675f890be4b6b12f1893
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-26 18:19:41 +1100

    Have many database strategies available

--- lib/throne/database.rb
+++ lib/throne/database.rb
@@ -1,17 +1,60 @@
 class Throne::Database  
-  class NameError < StandardError; end
+  class NotSetup < StandardError; end
+  class URIError < StandardError; end
   
   class << self
-    # Create the database (Throne.database)
-    def create
-      Throne::Request.put
+    @@databases = {}
+    
+    # Setup a database
+    # @params [Symbol] The name of the connection 
+    # @params [String] The uri of the database
+    def setup(name, uri)
+      raise URIError, "not a valid database URI" unless valid?(uri)
+      @@databases[name] = uri
+    end
+    
+    # Is the named connection setup? 
+    # @params [Symbol] The name of the connection
+    # @return [TrueClass]
+    def setup?(name)
+      @@databases.key? name
+    end
+    
+    # Create the database
+    # @params [Symbol] Name of the database to use (that has been setup using the setup method)
+    def create(db)
+      Throne::Request.put :database => db
     rescue RestClient::RequestFailed => e
       super unless e.message =~ /412$/
     end
     
-    # Destroy the database (Throne.database)
-    def destroy
-      Throne::Request.delete
+    # Destroy the database
+    # @params [Symbol] Name of the database to use (that has been setup using the setup method
+    # @return [TrueClass]
+    def destroy(db)
+      Throne::Request.delete :database => db
+    end
+    
+    # List the databases that you've :setup
+    # @return [Hash]
+    def list
+      @@databases
+    end
+    
+    # Retrieve a named database URI
+    def [](name)
+      raise NotSetup, "#{name} has not been setup. Check the docs for Throne::Database.setup" unless @@databases.key? name
+      @@databases[name]
+    end
+    
+    private
+    
+    # Checks weather the URI is valid
+    # URIs must be http or https
+    # @params [String] A URI
+    # @return [TrueClass]
+    def valid?(uri)
+      ["http", "https"].include?(URI.parse(uri).scheme)
     end
   end
 end
--- spec/throne/database_spec.rb
+++ spec/throne/database_spec.rb
@@ -1,38 +1,64 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Throne::Database do
-  describe "with nothing set" do
-    it "should raise an error" do
-      Throne.database = nil
-      lambda { Throne::Database.create }.should raise_error(Throne::Database::NameError)
+  describe "setup" do
+    it "should respond to setup" do
+      Throne::Database.should respond_to :setup
+    end
+    
+    it "should validate the database url on setup" do
+      lambda { Throne::Database.setup(:validate, "git://git-repo.com") }.should raise_error(Throne::Database::URIError)
+      lambda { Throne::Database.setup(:validate, "http://127.0.0.1/throne-valid") }
+    end
+    
+    it "should list all the registered databases" do
+      Throne::Database.setup(:default, "http://127.0.0.1/throne-specs")
+      Throne::Database.list.should be_an_instance_of(Hash)
+      Throne::Database.list.should include({:default => "http://127.0.0.1/throne-specs"})
+    end
+    
+    it "should return the named database" do
+      Throne::Database.setup(:validate, "http://127.0.0.1/throne-valid")
+      Throne::Database[:validate].should == "http://127.0.0.1/throne-valid"
+    end
+    
+    it "should raise when attempting to retrieve a database that has not been setup" do
+      lambda { Throne::Database[:not_setup] }.should raise_error(Throne::Database::NotSetup)
+    end
+    
+    it "should be setup" do
+      Throne::Database.setup?(:default).should be_true
+    end
+    
+    it "should not be setup" do
+      Throne::Database.setup?(:not_setup).should be_false
     end
   end
   
   describe "with a database set" do
     before :all do
-      @db_name = "throne-database-set"
-      Throne.database = @db_name
+      Throne::Database.setup(:throne_database_specs, "http://127.0.0.1:5984/throne-database-specs")
       
       begin
-        Throne::Database.destroy
+        Throne::Database.destroy(:throne_database_specs)
       rescue RestClient::ResourceNotFound
       end
     end
     
     it "should create the database" do
-      Throne::Database.create
-      lambda { RestClient.get("http://127.0.0.1:5948/#{@db_name}") }.should_not raise_error(RestClient::ResourceNotFound)
+      Throne::Database.create(:throne_database_specs)
+      lambda { RestClient.get("http://127.0.0.1:5948/throne-database-specs") }.should_not raise_error(RestClient::ResourceNotFound)
     end
     
     it "should destroy the database" do
-      Throne::Database.create
-      Throne::Database.destroy
-      lambda { Throne::Database.destroy }.should raise_error(RestClient::ResourceNotFound)
+      Throne::Database.create(:throne_database_specs)
+      Throne::Database.destroy(:throne_database_specs).should be_true
+      lambda { Throne::Database.destroy(:throne_database_specs) }.should raise_error(RestClient::ResourceNotFound)
     end
     
     it "should not error when creating a database that exists" do
-      Throne::Database.create
-      lambda { Throne::Database.create }.should_not raise_error
+      Throne::Database.create(:throne_database_specs)
+      lambda { Throne::Database.create(:throne_database_specs) }.should_not raise_error
     end
   end
 end
