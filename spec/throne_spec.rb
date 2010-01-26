ca4947b9aeaee4f4ec86cbfc32f46a4d03969e7e
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-26 18:20:10 +1100

    Self explanatory

--- spec/throne_spec.rb
+++ spec/throne_spec.rb
@@ -1,33 +1,5 @@
 require 'spec_helper'
 
 describe Throne do
-  it "should respond to server" do
-    Throne.should respond_to :server
-  end
-  
-  it "should respond to database" do
-    Throne.should respond_to :database
-  end
-  
-  it "should respond to server=" do
-    Throne.should respond_to "server="
-  end
-  
-  it "should respond to database=" do
-    Throne.should respond_to "database="
-  end
-  
-  it "should default the server to 127.0.0.1:5984" do
-    Throne.server.should == "http://127.0.0.1:5984"
-  end
-  
-  it "should set the server" do
-    Throne.server = "http://127.0.0.1:5984"
-    Throne.server.should == "http://127.0.0.1:5984"
-  end
-
-  it "should set the database" do
-    Throne.database = "appname_environment"
-    Throne.database.should == "appname_environment"
-  end  
+  # Nothing to see anymore
 end
